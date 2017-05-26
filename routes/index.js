731ea16d3b22b10fc3bb9c1016e76dc0fd78a425
Julian Mullins <mullinsjulian@gmail.com>
2017-05-26 16:39:28 -0700

    initial pagination and styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+//random language 'hello' text generator
+var randomHello = function() {
+	var arr = ["hello", "namaste", "hola", "salut", "ciao", "Hallo", "nǐ hǎo", "Ahlan wa Sahlan", "shalom", "marhaba", "olá", "kon'nichiwa", "annyeonghaseyo"];
+	var randNum = Math.floor(Math.random()*arr.length);
+	return arr[randNum];
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('index', {
+  	title: 'Julian Mullins',
+  	subtitle: "Entrepreneur, Developer & Designer",
+  	hello: randomHello()
+  });
 });
 
 module.exports = router;
