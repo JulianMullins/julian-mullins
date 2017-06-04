5d83c7c0e06577f0db9507cb4241da9d5ab24db9
Julian Mullins <mullinsjulian@gmail.com>
2017-06-04 14:35:48 -0700

    front-loading images

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -67,37 +67,37 @@ $(document).ready(function() {
 				photo.fadeOut('slow', function() {
 					photo.attr('src', "../images/costa-rica.jpg");
 				})
-				photo.fadeIn('slow');
+				photo.fadeIn('fast');
 				break;
 			case 1:
 				photo.fadeOut('slow', function() {
 					photo.attr('src', "../images/spain.jpg");
 				});
-				photo.fadeIn('slow');
+				photo.fadeIn('fast');
 				break;
 			case 2:
 				photo.fadeOut('slow', function() {
 					photo.attr('src', "../images/break-dance.jpg");
 				});
-				photo.fadeIn('slow');
+				photo.fadeIn('fast');
 				break;
 			case 3:
 				photo.fadeOut('slow', function() {
 					photo.attr('src', "../images/trance.jpg");					
 				})
-				photo.fadeIn('slow');
+				photo.fadeIn('fast');
 				break;
 			case 4:
 				photo.fadeOut('slow', function() {
 					photo.attr('src', "../images/ballet.jpg");					
 				})
-				photo.fadeIn('slow');
+				photo.fadeIn('fast');
 				break;
 			default:
 				photo.fadeOut('slow', function() {
 					photo.attr('src', "../images/profile-pic.jpg");					
 				})
-				photo.fadeIn('slow');
+				photo.fadeIn('fast');
 		}
 
 		factoidCount++;
